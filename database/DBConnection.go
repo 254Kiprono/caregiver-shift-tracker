689b0394f1e1d7bbad622b0b7bb0d3e0b3202cc4
254Kiprono <beralynebera@gmail.com>
2025-06-28 19:50:54 +0300

    changes

--- database/DBConnection.go
+++ database/DBConnection.go
@@ -30,7 +30,7 @@ func InitializeDB(cfg *config.Config) (*gorm.DB, error) {
 	}
 
 	// Perform automatic migration for the User model
-	err = db.AutoMigrate(&models.User{}, models.Task{}, models.Schedule{})
+	err = db.AutoMigrate(&models.User{}, models.Schedule{}, models.Task{})
 	if err != nil {
 		log.Fatalf("failed to auto-migrate User model: %v", err)
 	}
